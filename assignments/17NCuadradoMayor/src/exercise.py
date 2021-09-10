c1fa279ab8a55e94da0129ddded8004ede0f6b82
A01252396 <A01252396@ITESM.MX>
2021-09-10 17:53:05 +0000

    Examen Parcial_Parte Práctica 2 (40%)

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,8 +1,11 @@
-
-
+import math
 def main():
     numero = int(input("Escribe un numero : "))
     #escribe tu código abajo de esta línea
+    n=1
+    while numero>=n**2:
+     n=n+1
+    print(n)    
 
 
 if __name__=='__main__':
